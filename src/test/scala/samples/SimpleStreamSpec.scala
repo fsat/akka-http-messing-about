cd52af4808f97e494d32eab25569d0b9c9b8cb80
Felix Satyaputra <felix.satyaputra@typesafe.com>
2015-07-31 23:54:05 +1000

    Trying to setup precanned stream

--- src/test/scala/samples/SimpleStreamSpec.scala
+++ src/test/scala/samples/SimpleStreamSpec.scala
@@ -1,17 +1,17 @@
 package samples
 
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{RunnableGraph, Keep, Sink, Source}
 import org.scalatest.FunSpec
 import org.scalatest.concurrent.ScalaFutures
 
 import scala.concurrent.Future
 
 class SimpleStreamSpec extends FunSpec with TestBase with ScalaFutures {
-  describe("simple stream") {
-    describe("simple Source") {
-      val source = Source(1 to 20)
-      val multipleByTwo: Int => Int = _ * 2
+  val source = Source(1 to 20)
+  val multipleByTwo: Int => Int = _ * 2
 
+  describe("simple stream from simple Source") {
+    describe("from simple Source") {
       it("can be mapped over and poured into a Sink") {
         val result = source
           .map(multipleByTwo)
@@ -29,6 +29,16 @@ class SimpleStreamSpec extends FunSpec with TestBase with ScalaFutures {
 
         result.futureValue shouldBe 420
       }
+    }
+
+    it("can be setup as a pre-canned processing pipeline") {
+      val processingPipeline: Source[Int, Unit] => RunnableGraph[Future[Int]] =
+        _.mapAsync(parallelism = 3) { value =>
+          Future.successful(multipleByTwo(value))
+        }
+        .toMat(Sink.fold(0) { _ + _})(Keep.right)
+
+      processingPipeline(source).run().futureValue shouldBe 420
     }
   }
 }
